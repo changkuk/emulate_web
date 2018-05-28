11cfbeb278849b09aedef8bd2e71c6ae462d08c8
Kena Alexander <kenaxle@gmail.com>
2018-05-28 23:17:25 +0900

    test

--- after_school.py
+++ after_school.py
@@ -13,6 +13,7 @@ import time
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+sleep(1)
 
 loginUrl = 'http://myns.cafe24.com/afterschool/regi/log/_login.php'
 pageUrl = 'http://myns.cafe24.com/afterschool/main/main/gate.php'
